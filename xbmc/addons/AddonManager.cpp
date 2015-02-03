dc80c9668cb406589b35d59f271fc551e80cf8a3
hongwei.shi <hongwei.shi@vidon.me>
2015-02-03 13:46:35 +0800

    [misc]fix build error

--- xbmc/addons/AddonManager.cpp
+++ xbmc/addons/AddonManager.cpp
@@ -277,9 +277,9 @@ bool CAddonMgr::Init()
   status = m_cpluff->register_pcollection(m_cp_context, CSpecialProtocol::TranslatePath("special://home/addons").c_str());
   status = m_cpluff->register_pcollection(m_cp_context, CSpecialProtocol::TranslatePath("special://xbmc/addons").c_str());
 	status = m_cpluff->register_pcollection(m_cp_context, CSpecialProtocol::TranslatePath("special://xbmcbin/addons").c_str());
-#if defined(TARGET_ANDROID) && defined(HAS_VIDONME)
-	status = m_cpluff->register_pcollection(m_cp_context, getenv("XBMC_MORE_ADDONS"));
-#endif
+//#if defined(TARGET_ANDROID) && defined(HAS_VIDONME)
+//	status = m_cpluff->register_pcollection(m_cp_context, getenv("XBMC_MORE_ADDONS"));
+//#endif
   if (status != CP_OK)
   {
     CLog::Log(LOGERROR, "ADDONS: Fatal Error, cp_register_pcollection() returned status: %i", status);

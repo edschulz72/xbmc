f27c72bb9708e6499b5ec87c0d5a4b2b732fa040
yingjie.li <yingjie.li@vidon.me>
2016-04-29 12:25:56 +0800

    [dev] for resume string can not showing in Chinese in android.

--- xbmc/video/windows/GUIWindowVideoBase.cpp
+++ xbmc/video/windows/GUIWindowVideoBase.cpp
@@ -1000,7 +1000,12 @@ std::string CGUIWindowVideoBase::GetResumeString(const CFileItem &item)
   GetResumeItemOffset(&item, startOffset, startPart);
   if (startOffset > 0)
   {
+#ifdef HAS_VIDONME
+	  resumeString = g_localizeStrings.Get(12022);
+	  StringUtils::Replace(resumeString, "%s", StringUtils::SecondsToTimeString(startOffset / 75));
+#else
     resumeString = StringUtils::Format(g_localizeStrings.Get(12022).c_str(), StringUtils::SecondsToTimeString(startOffset/75).c_str());
+#endif
     if (startPart > 0)
     {
       std::string partString = StringUtils::Format(g_localizeStrings.Get(23051).c_str(), startPart);
